d11c7490a1c82286bb63331fa18a2dffb4a4e869
OmoriRiku <oomori3321@gmail.com>
2024-07-24 23:05:16 +0900

    commit

--- Java/アルゴリズム学習用/makeNewArray/Main.java
+++ Java/アルゴリズム学習用/makeNewArray/Main.java
@@ -4,13 +4,19 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
+		//	インスタンス生成
 		MakeArray makeArray = new MakeArray();
 
+		//	引数用の配列in
 		int[] in = {3, 2, 1, 6, 5, 4};
+
+		//	ループ制御用
 		int i;
 
+		//	MakeArrayクラスのmakeNewArrayメソッドの呼び出し
 		List<Integer> out = makeArray.makeNewArray(in);
 
+		//	結果を表示
 		for(i = 0; i < out.size(); i++) {
 			System.out.println(out.get(i));
 		}
--- Java/アルゴリズム学習用/makeNewArray/MakeArray.java
+++ Java/アルゴリズム学習用/makeNewArray/MakeArray.java
@@ -1,5 +1,5 @@
 /*
-	引数で渡された配列の要素を一つずつ足した値で配列を作る
+	引数で渡された配列の要素を一つずつ足した値でListを作る
 	in {3, 2, 1...} → out {3, 5, 6...}
 */
 
@@ -9,19 +9,25 @@ import java.util.ArrayList;
 public class MakeArray {
 
 	public List<Integer> makeNewArray(int[] in) {
+		//	戻り値用のList
 		List<Integer> out = new ArrayList<>();
 
+		//	ループ制御
 		int i;
 
+		//	仮に格納する変数
 		int tail;
 
+		//	in配列のインデックス0の値を追加
 		out.add(in[0]);
 
+		//	配列の要素を一つずつ足した値をListに追加する
 		for (i = 1; i < in.length; i++) {
 			tail = out.get(out.size() - 1);
 			out.add(tail + in[i]);
 		}
 
+		//	追加後のListを返却
 		return out;
 	}
 
